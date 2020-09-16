45ac498674a1581e8eb02de87db7e913ecb46b21
ketrab2003 <bartekkrawisz@wp.pl>
2020-09-16 16:40:25 +0200

    Added initial Tasker class

---
+++ tasker.py
@@ -0,0 +1,29 @@
+import asyncio
+
+class Tasker:
+    def __init__(self):
+        self._queue = asyncio.Queue()
+        self.running = False    
+
+    async def addJob(self, coro):
+        """ Add new coroutine to task queue """
+        if not asyncio.iscoroutine(coro):
+            raise ValueError("a coroutine was expected, got {!r}".format(coro))
+        
+        await self._queue.put(coro)
+    
+    async def _loop(self):
+        while 1:
+            coro = await self._queue.get()
+            try:
+                asyncio.run(coro)
+            except Exception as e:
+                print(f"{self} Error: {e}")
+
+    async def start(self):
+        """ Return infinite task to asyncio.gather with other tasks in program """
+        if self.running:
+            return None # TODO: Add already running exception
+        
+        self.running = True
+        return asyncio.create_task(self._loop())
